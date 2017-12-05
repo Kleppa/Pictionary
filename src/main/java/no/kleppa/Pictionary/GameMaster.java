751c4d7ffdc494d46369aea3361e847d427ab322
Kleppa <jarandwaagekleppa@gmail.com>
2017-12-05 12:53:36 +0100

    Reset canvas now possible

--- src/main/java/no/kleppa/Pictionary/GameMaster.java
+++ src/main/java/no/kleppa/Pictionary/GameMaster.java
@@ -1,11 +1,12 @@
 package no.kleppa.Pictionary;
 
-import javafx.event.EventHandler;
+
 import javafx.geometry.Insets;
 import javafx.scene.Parent;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.*;
+import javafx.scene.control.Button;
 
 import javafx.scene.canvas.Canvas;
 import javafx.scene.paint.Color;
@@ -17,12 +18,32 @@ public class GameMaster {
 	}
 
 	private void setUpRootPane() {
+		BorderPane subPane= new BorderPane();
 		Canvas canvas = new Canvas(400,500);
 		GraphicsContext gc = canvas.getGraphicsContext2D();
 		gc.setFill(Color.WHITE);
-		gc.fillRect(10, 75, canvas.getWidth(), canvas.getHeight());
+		gc.fillRect(0, 0, canvas.getWidth(), canvas.getHeight());
 
 		gc.setStroke(Color.BLACK);
+		canvasFunctionality(canvas,gc);
+
+
+		rootPane.setBackground(
+				new Background(
+						new BackgroundFill(Color.WHITE, CornerRadii.EMPTY, Insets.EMPTY)
+				)
+		);
+		Button resetButton= new Button();
+		resetButton.setText("Reset Canvas");
+		resetCanvas(resetButton,gc,canvas);
+		subPane.setBottom(resetButton);
+		subPane.setCenter(canvas);
+		rootPane.setBackground(new Background(new BackgroundFill(Color.SLATEGREY, CornerRadii.EMPTY, Insets.EMPTY)));
+		rootPane.setCenter(subPane);
+
+	}
+
+	private void canvasFunctionality(Canvas canvas, GraphicsContext gc) {
 		canvas.addEventHandler(MouseEvent.MOUSE_DRAGGED,
 				event -> {
 					gc.lineTo(event.getX(), event.getY());
@@ -34,17 +55,13 @@ public class GameMaster {
 					gc.moveTo(event.getX(), event.getY());
 					gc.stroke();
 				});
-
-		rootPane.setBackground(
-				new Background(
-						new BackgroundFill(Color.CRIMSON, CornerRadii.EMPTY, Insets.EMPTY)
-				)
-		);
-
-		rootPane.setCenter(canvas);
-
 	}
+	private void resetCanvas(Button resetButton,GraphicsContext gc, Canvas canvas){
+		resetButton.setOnAction(e->{
+			gc.fillRect(0, 0, canvas.getWidth(), canvas.getHeight());
 
+		});
+	}
 
 	public Parent getRootPane() {
 		return rootPane;
